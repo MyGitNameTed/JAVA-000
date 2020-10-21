3796039acac38684d67d8f57178b204b1c098101
lishihao <lshlsyx@163.com>
2020-10-21 23:53:18 +0800

    极客时间 训练营 第一周 20201021

---
+++ Week_01/geekbang-week01/src/main/java/com/learn/geekbang/week01/experiment/Hello.java
@@ -0,0 +1,7 @@
+package com.learn.geekbang.week01.experiment;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Class Initialized!");
+    }
+}
